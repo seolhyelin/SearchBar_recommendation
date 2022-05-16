571d503e3db5fd7433e8d8cb17704ee7bf2e3dd3
seolhyelin <tosiltosilkk@gmail.com>
2022-05-16 13:19:45 +0900

    First Commit

---
+++ src/routes/index.jsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from 'react-router-dom'
+import styles from './Routes.module.scss'
+
+const App = () => {
+  return (
+    <div className={styles.appWrapper}>
+      <div className={styles.app} />
+    </div>
+  )
+}
+
+export default App
